effc32ec90c02dcdf786bf134813cf1b8a72d4a3
kazuma104 <kazuma@info.nara-k.ac.jp>
2019-12-06 20:11:39 +0900

    Aproblem solved

---
+++ Aproblem.py
@@ -0,0 +1,15 @@
+A,B = map(int, input().split())
+C,D = map(int, input().split())
+ 
+if D == 1:
+  print("1")
+else:
+  print("0")
+A,B = map(int, input().split())
+C,D = map(int, input().split())
+
+if D == 1:
+  print("1")
+else:
+  print("0")
+提出情報
