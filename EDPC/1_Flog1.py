8b854b059068658bd69a70dc558c491939c13ec6
kazuma104 <kazuma@info.nara-k.ac.jp>
2019-12-24 19:47:30 +0900

    Q1~2 is completed!

--- EDPC/1_Flog1.py
+++ EDPC/1_Flog1.py
@@ -1,1 +1,9 @@
-abc = 1
+N = int(input())
+h = list(map(int, input().split()))
+
+dp = [[0]*(3) for i in range(N+1)]
+
+print(*dp, sep="\n") #dp部確認用
+for i in range(N):
+    for j in range(3):
+        print("a")
