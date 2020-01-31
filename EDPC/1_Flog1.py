f209325bbd1b02f60aa8eaffa35eebd24db8d61a
kazuma104 <kazuma@info.nara-k.ac.jp>
2020-01-31 15:30:26 +0900

    new function added.

--- EDPC/1_Flog1.py
+++ EDPC/1_Flog1.py
@@ -3,6 +3,7 @@ N = int(input())
 h = list(map(int, input().split()))
 inf = float("inf")
 dp = [inf]*(N)
+print(dp)
 
 #2:初期値
 dp[0] = 0
@@ -13,5 +14,13 @@ for i in range(1,N):
     if(i>1): #2つ目の足場には二個前の足場がないので．
         dp[i] = min(dp[i], dp[i-2] + abs(h[i] - h[i-2]))
 
+# #3:dp実行部(配るDP)
+# for i in range(N):
+#     if(i+1<N):
+#         dp[i+1] = min(dp[i+1], dp[i] + abs(h[i] - h[i+1]))
+#     if(i+2<N):
+#         dp[i+2] = min(dp[i+2], dp[i] + abs(h[i] - h[i+2]))
+
 #4:出力
+print(dp)
 print(dp[N-1])
