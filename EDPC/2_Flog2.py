07079a615efde768e2664d53c6e406b8d2e25cf8
kazuma104 <kazuma@info.nara-k.ac.jp>
2020-02-17 21:05:54 +0900

    faster.

--- EDPC/2_Flog2.py
+++ EDPC/2_Flog2.py
@@ -1,16 +1,23 @@
-#1:初期化
-N, K = map(int,input().split())
-#h = list(map(int, input().split()))
-h = [int(x) for x in input().split()] #こっちのほうが早い？
-dp = [0]*(N)
+import sys
+input = sys.stdin.readline
 
-#2:初期値
-dp[0] = 0
+def main():
+    #1:初期化
+    N, K = map(int,input().split())
+    #h = list(map(int, input().split()))
+    h = [int(x) for x in input().split()] #こっちのほうが早い？
+    dp = [0]*(N)
 
-#3:dp実行部(貰うDP)
-for i in range(1,N):
-    L = [dp[j]+abs(h[i]-h[j]) for j in range(max(0,i-K),i)]
-    dp[i] = min(L)
+    #2:初期値
+    dp[0] = 0
 
-#4:出力
-print(dp[N-1])
+    #3:dp実行部(貰うDP)
+    for i in range(1,N):
+        L = [dp[j]+abs(h[i]-h[j]) for j in range(max(0,i-K),i)]
+        dp[i] = min(L)
+
+    #4:出力
+    print(dp[N-1])
+
+if __name__ == '__main__':
+    main()
