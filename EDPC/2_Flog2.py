7a469012f0f94f1d3a4f417f08fcd9b866aae386
kazuma104 <kazuma@info.nara-k.ac.jp>
2020-02-20 11:30:48 +0900

    change files.

--- EDPC/2_Flog2.py
+++ EDPC/2_Flog2.py
@@ -1,17 +1,19 @@
+#Flog1のやり方をそのまま応用してもできるが，その場合間に合わないやつがある
+#やっていることじたいはほぼ変わりないが，初期値との比較は行っていないためその分速い（？）
+
 import sys
 input = sys.stdin.readline
 
 def main():
     #1:初期化
     N, K = map(int,input().split())
-    #h = list(map(int, input().split()))
-    h = [int(x) for x in input().split()] #こっちのほうが早い？
+    h = list(map(int, input().split()))
     dp = [0]*(N)
 
     #2:初期値
-    dp[0] = 0
+    dp[0] = 0 #なくてもよい
 
-    #3:dp実行部(貰うDP)
+    #3:dp実行部(貰うDP)(配るDPでは事項時間制限に間に合わない)
     for i in range(1,N):
         L = [dp[j]+abs(h[i]-h[j]) for j in range(max(0,i-K),i)]
         dp[i] = min(L)
