6308e18e1c070b2916aaa3df66107d197af8cd3a
kazuma104 <kazuma@info.nara-k.ac.jp>
2020-02-27 20:53:21 +0900

    BC151 is finished.

--- test.py
+++ test.py
@@ -1,45 +1,53 @@
-# 155 D - Pairs
+from collections import deque
 
-import numpy as np
+h,w = map(int, input().split())
+l = [list(input()) for _ in range(h)]
 
-N, K = map(int, input().split())
-A = np.array(sorted(list(map(int, input().split()))), np.int64)
+# 最大値の格納する
+ans = 0
 
-n_zero = len(A[A == 0])
-A_posi = A[A > 0]
-A_nega = A[A < 0]
+def bfs(y,x,m):#未実装
+    #表の初期化
+    graph = [[None] * w for i in range(h)]
+    #最初の地点のコストは0
+    graph[y][x] = 0
 
-print(n_zero, A_posi, A_nega)
+    q = deque()
+    q.append((y,x))
 
-def n_pairs_less_than_or_equal_to_border(product_border):
-    n_pairs = -np.sum(A*A <= product_border) # i==jかつ条件を満たすペアの個数を先に引き、以降重複を許して数える
-    print("a",n_pairs)
-    n_pairs += n_zero * N * (product_border >= 0)
-    print("b",n_pairs)
-    n_pairs += np.searchsorted(A, product_border//A_posi, side="right").sum()
-    print("c",n_pairs)
-    n_pairs += (N - np.searchsorted(A, -(-product_border//A_nega), side="left")).sum()
-    print("d",n_pairs)
-    # 上記2行は下記と同様の処理だが下記の実装だとTLE
-    # for i in range(N):
-    #     if A[i] > 0:
-    #         n_pairs += np.searchsorted(A, product_border//A[i], side="right")
-    #     elif A[i] < 0:
-    #         n_pairs += N - np.searchsorted(A, -(-product_border//A[i]), side="left")
+    for i in range(h):
+        for j in range(w):
+            if l[i][j] == '#':
+                graph[i][j] = -1
 
-    n_pairs //= 2
-    return n_pairs
+    #ノードの移動方向 左が縦軸, 右が横軸
+    move = ((0, 1), (0, -1), (1, 0), (-1, 0))
 
+    while q: #wがからになるまで続ける while len(q):でもOK
+        #que左端から取り出す
+        i, j = q.popleft()
 
-lower = -(10**18)
-upper = 10**18
+        for di, dj in move:
+            #ni, nj 遷移先の座標
+            ni , nj = i + di, j + dj
+            #遷移先がフィールド内であるか、Noneかどうかをチェックする
+            if 0 <= ni < h and 0 <= nj < w and graph[ni][nj] is None:
+                #遷移可能であれば現在のコストに1足して渡す
+                #print(type(graph[i][j]))
+                graph[ni][nj] = graph[i][j] + 1
+                #queに格納する
+                q.append((ni, nj))
 
-while lower <= upper:
-    x = (lower + upper) // 2
-    if n_pairs_less_than_or_equal_to_border(x) >= K:
-        ans = x
-        upper = x - 1
-    else:
-        lower = x + 1
+    #queが空になると移動コストtableが完成
+    #表中の最大値返す
+    return max(max(r) for r in graph)
 
+for i in range(h):
+    for j in range(w):
+        #'.'であればスタート地点になり得るので、それをbfsにかける
+        if l[i][j] == '.':
+            d = bfs(i,j,l) # (x, y) = (j, i), lは入力の表の情報
+            #返り値が現在の最大値より大きければ値を更新する
+            if ans < d:
+                ans = d
 print(ans)
