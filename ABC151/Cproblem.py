1d9328802cde1540ce19e468a0aaabbf3a690fdb
kazuma104 <kazuma@info.nara-k.ac.jp>
2020-02-27 13:57:16 +0900

    change files.

--- ABC151/Cproblem.py
+++ ABC151/Cproblem.py
@@ -1,11 +1,23 @@
+#ACしてないときはWAは加算されないようにする必要あり．
 N, M = map(int, input().split())
 
-judge = []
-A = [[0]*3 for i in range(N)]
-for i in range(M):
-    p, S = input().split()
-  #  if(A[p-1,2] == 0):
+AC = 0
+WA = 0
+A = [[0]*2 for _ in range(N)] #flag管理,n番目の問題がACになったらn-1番目の配列に1を入れる
+for _ in range(M):
+	p, S = input().split() #注意!ここのpはstrで入っている
+	ip = int(p)
+	if A[ip-1][1] == 0:
+		if S == "WA":
+			A[ip-1][0] += 1
+		else:
+			A[ip-1][1] = 1
+			AC += 1
+	else:
+		continue
 
+for i in range(N): #WAのカウント
+	if A[i][1] == 1:
+		WA += A[i][0]
 
-
-
+print(AC,WA)
