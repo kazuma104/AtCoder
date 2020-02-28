085d5c6abf51b3c2f635ffa48f4d6e33177f73de
kazuma104 <kazuma@info.nara-k.ac.jp>
2020-02-28 13:38:08 +0900

    ABC149 is finidhed.

---
+++ ABCproblems/ABC149/Cproblem.py
@@ -0,0 +1,22 @@
+def prime_factorize(n):
+    a = []
+    while n % 2 == 0:
+        a.append(2)
+        n //= 2
+    f = 3
+    while f * f <= n:
+        if n % f == 0:
+            a.append(f)
+            n //= f
+        else:
+            f += 2
+    if n != 1:
+        a.append(n)
+    return a
+
+X = int(input())
+while True:
+    if len(prime_factorize(X)) == 1:
+        break
+    X += 1
+print(X)
---
+++ ABCproblems/ABC149/Dproblem.py
@@ -0,0 +1,21 @@
+N, K = map(int, input().split())
+R, S, P = map(int, input().split())
+T = input()
+M = ""
+# score = 0
+# for i in range(N):
+#     if i >= K:
+#         if T[i] == "r" and M[i-K] == "p":
+
+#     else:
+#         if T[i] == "r":
+#             score += P
+#             M += "p"
+#         elif T[i] == "s":
+#             score += R
+#             M += "r"
+#         elif T[i] == "p":
+#             score += S
+#             M += "s"
+
+# print(M)
