b1463eb67de963764adcc995e92c102fec627569
kazuma104 <kazuma@info.nara-k.ac.jp>
2020-04-04 20:58:51 +0900

    ABC160 is finished.

--- ABCproblems/ABC160/Cproblem.py
+++ ABCproblems/ABC160/Cproblem.py
@@ -4,11 +4,11 @@ A = list(map(int, input().split()))
 lenmax = 0
 for i in range(N-1):
     a = A[i+1] - A[i]
-    print(a)
+    #print(a)
     if a > lenmax:
         lenmax = a
 
-print(A[len(A)-1],A[0])
+#print(A[len(A)-1],A[0])
 if K - (A[len(A)-1] - A[0]) > lenmax:
     lenmax = K - (A[len(A)-1] - A[0])
 
