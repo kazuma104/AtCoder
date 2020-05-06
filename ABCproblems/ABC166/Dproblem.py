ea8e1bd4b19c07929679c7be55bda7ad24c50ac6
kazuma104 <kazuma@info.nara-k.ac.jp>
2020-05-06 16:07:30 +0900

    Dproblem in ABC166 is changed.

--- ABCproblems/ABC166/Dproblem.py
+++ ABCproblems/ABC166/Dproblem.py
@@ -1,26 +1,11 @@
-def factorization(n):
-    arr = []
-    temp = n
-    for i in range(2, int(-(-n**0.5//1))+1):
-        if temp%i==0:
-            cnt=0
-            while temp%i==0:
-                cnt+=1
-                temp //= i
-            arr.append([i, cnt])
-
-    if temp!=1:
-        arr.append([temp, 1])
-
-    if arr==[]:
-        arr.append([n, 1])
-
-    return arr
 X = int(input())
-soin = factorization(X)
-small = soin[0][0]
 
-for i in range(10**9):
-    if i**5-(i-small)**5==X:
-        print(i,i-small)
+flag = False
+for a in range(-118,120):
+    for b in range(-119,119):
+        if a**5-b**5==X:
+            print(a,b)
+            flag = True
+            break
+    if flag:
         break
